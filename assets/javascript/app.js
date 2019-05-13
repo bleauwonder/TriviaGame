73166abdc628ba5feac7d8a0f0a58b7348eade07
bleauwonder <elyzableau.om@gmail.com>
2019-05-12 21:32:24 -0400

    trial js, adding css and html elements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,6 +25,7 @@ function run() {
       clearInterval(intervalId);
     }
     run();
+});
 
 // Quiz
 var myQuestions = [
@@ -47,15 +48,65 @@ var myQuestions = [
 		correctAnswer: 'c'
 	}
 ];
+var quizContainer;
+var resultsContainer;
+var submitButton;
+
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
 	function showQuestions(questions, quizContainer){
-		// code will go here
+        var output = [];
+        var answers;
+        // for each question
+        for(var i = 0; i < questions.length; i++) {
+            answers = [];
+            for(letter in questions[i].answers) {
+                // $("input['question' + i +]:checked").val(letter);
+                // letter
+                answers.push('<label>' + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                    + letter + ': ' + questions[i].answers[letter] + '</label>')
+            }
+        }
 	}
+        output.push('<div class="question">' + questions[i].question + '</div>'
+            + '<div class="answers">' + answers.join('') + '</div>'
+        );
+    }
+    quizContainer.innerHTML = output.join('');
+   
 
 	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
+	// gather answer containers from our quiz
+	var answerContainers = quizContainer.querySelectorAll('.answers');
+	
+	// keep track of user's answers
+	var userAnswer = '';
+	var numCorrect = 0;
+	
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+
+		// find selected answer
+		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+		
+		// if answer is correct
+		if(userAnswer===questions[i].correctAnswer){
+			// add to the number of correct answers
+			numCorrect++;
+			
+			// color the answers green
+			answerContainers[i].style.color = 'lightgreen';
+		}
+		// if answer is wrong or blank
+		else{
+			// color the answers red
+			answerContainers[i].style.color = 'red';
+		}
 	}
+	// show number of correct answers out of total
+	resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
+	
 
 	// show the questions
 	showQuestions(questions, quizContainer);
@@ -63,9 +114,13 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 	// when user clicks submit, show results
 	submitButton.onclick = function(){
 		showResults(questions, quizContainer, resultsContainer);
-	}
-}
+    }
+    
+    $('quiz').html(quizContainer);
+    $('results').html(resultsContainer);
+    $('submit').html(submitButton);
 
+    generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 });
 
 // On load, the perseon will see the title of the Trivia Game and a Start button to click
