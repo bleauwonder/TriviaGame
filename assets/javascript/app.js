cd848601286192204af2a63e6181a0fb2d59ee31
bleauwonder <elyzableau.om@gmail.com>
2019-05-14 16:11:59 -0400

    started turning questions into slides

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,29 +3,30 @@ $(document).ready(function() {
     $(".startBtn").on("click", function(e) {
         $("#quiz").html(generateQuiz(questions, quizContainer, resultsContainer, submitButton));
 
-// Countdown clock
-var number = 60;
-var intervalId;
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  }
-  function decrement() {
-    number--;
-    $("#show-number").html("<h2>" + number + "</h2>");
-    if (number === 0) {
-      stop();
-      alert("Time's Up!")
-    }
-  }
-    function stop() {
-      clearInterval(intervalId);
-    }
-    run();
-});
+    // Countdown clock
+        var number = 60;
+        var intervalId;
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+        }
+        function decrement() {
+            number--;
+            $("#show-number").html("<h2>" + number + "</h2>");
+        if (number === 0) {
+            stop();
+            alert("Time's Up!")
+            }
+        }
+        function stop() {
+            clearInterval(intervalId);
+        }
+        run();
+    });
 
 // Quiz
-var myQuestions = [
+    // Set up the Questions array
+    var myQuestions = [
 	{
 		question: "Who launched Sun Records in February of 1952?",
 		answers: {
@@ -45,45 +46,157 @@ var myQuestions = [
             d: 'It’s a simple name for simply great music'
 		},
 		correctAnswer: 'a'
+    },
+    {
+		question: "What was the main style of music that Sun Records recorded initially?",
+		answers: {
+			a: 'Gospel',
+			b: 'Blues',
+            c: 'Country',
+            d: 'Anyone who would come in'
+		},
+		correctAnswer: 'd'
+    },
+    {
+		question: "Who was the first big talent signed to Sun?",
+		answers: {
+			a: 'Johnny Cash',
+			b: 'Roy Orbison',
+            c: 'Elvis Presley',
+            d: 'Conway Twitty'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "Who were considered the Million Dollar Quartet?",
+		answers: {
+			a: 'Elvis Presley, Carl Perkins, Johnny Cash, Jerry Lee Lewis',
+			b: 'Roy Orbison, Charlie Rich, Bill Justis, Harold Jenkins (a.k.a. Conway Twitty)',
+            c: 'Jerry Lee Lewis, Roy Orbison, Buddy Holly, Sam Cooke',
+            d: 'Elvis Presley, Johnny Cash, Sam Cooke, Chuck Berry'
+		},
+		correctAnswer: 'a'
+    },
+    {
+		question: "Why is “Rocket 88” considered one of the first rock and roll songs? (recorded at Sun Records 1951)",
+		answers: {
+			a: 'The producer jerry-rigged the speaker with packing paper and accidentally made the sound fuzzy',
+			b: 'It had double string electric guitar riffs that no one had recorded before',
+            c: 'It was the first single to have screams, shouts, and yelps over top a frenetic jump blues rhythm',
+            d: 'It was about fast cars and fast times set to a steady quick beat.'
+		},
+		correctAnswer: 'a'
+    },
+    {
+		question: "What was Johnny Cash told when he auditioned at Sun Records in 1954?",
+		answers: {
+			a: '“You are incredible, let’s get in the recording booth right away.”',
+			b: '“You aren’t what we are looking for, thank you.”',
+            c: '“Go home and sin, then come back with a song I can sell.”',
+            d: '“Please get some voice lessons."'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "What musical style did Sun Records eventually become best known for?",
+		answers: {
+			a: 'Country',
+			b: 'Rockabilly',
+            c: 'Gospel',
+            d: 'Rock and Roll'
+		},
+		correctAnswer: 'b'
+    },
+    {
+		question: "Shelby Singleton purchased Sun Records in 1969. For what label was he a producer?",
+		answers: {
+			a: 'Janus',
+			b: 'A&M”',
+            c: 'Neptune',
+            d: 'Mercury'
+		},
+		correctAnswer: 'd'
+    },
+   
+    {
+		question: "Where is Sun Records now located?",
+		answers: {
+			a: 'Los Angeles, California',
+			b: 'Austin, Texas',
+            c: 'Nashville, Tennessee',
+            d: 'Still in Memphis'
+		},
+		correctAnswer: 'c'
 	}
 ];
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+    // Function to generate the quiz on the page
+    function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
-	function showQuestions(questions, quizContainer){
-        // we'll need a place to store the output and the answer choices
-        var output = [];
-        var answers;
-    
-        // for each question...
-        for(var i=0; i<questions.length; i++){
-            
-            // first reset the list of answers
-            answers = [];
-    
-            // for each available answer to this question...
-            for(letter in questions[i].answers){
-    
-                // ...add an html radio button
-                answers.push(
-                    '<label>'
+	    function showQuestions(questions, quizContainer) {
+        // blank array to push user answers into, and an correct answers array
+            var output = [];
+            var answers;
+            for(var i=0; i<questions.length; i++) {
+                answers = [];
+                // for each available answer to this question
+                for(letter in questions[i].answers) {
+                    // add an html radio button
+                    answers.push('<label>'
                         + '<input type="radio" name="question'+i+'" value="'+letter+'">'
                         + letter + ': '
-                        + questions[i].answers[letter]
-                    + '</label>'
-                );
+                        + questions[i].answers[letter] + '</label>'
+                    );
+                }
+                // add this question and its answers to the output
+                    output.push(`<div class="slide">
+                        <div class="question"> ${questions[i].question} </div>
+                        <div class="answers"> ${answers.join("")} </div>
+                        </div>`
+                    );
             }
-    
-            // add this question and its answers to the output
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-    
-        // finally combine our output list into one string of html and put it on the page
+        // output list into one string
         quizContainer.innerHTML = output.join('');
     }
+
+        var previousButton = $('#previous').get(0);
+            $('#previous').get[0];
+        var nextButton = $('#next').get(0);
+            $('#next').get[0];
+        var slides = $('.slide');
+        var currentSlide = 0;
+
+    function showSlide(n) {
+        $(slides[currentSlide]).removeClass('active-slide');
+        $(slides[n]).addClass('active-slide');
+        currentSlide = n;
+        if(currentSlide === 0) {
+            previousButton.style.display = 'none';
+        }
+        else {
+            previousButton.style.display = "inline-block";
+        }
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        }
+        else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    }
+    showSlide(0);
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
 
     showQuestions(questions, quizContainer);
 
@@ -135,6 +248,7 @@ var submitButton = document.getElementById('submit');
 generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
 });
+
 // On load, the person will see the title of the Trivia Game and a Start button to click
 // On click, the game starts with a 60 second countdown clock at the top
     // need a click even that will take the player to the quiz page, which looks different from the start page.
